36e59294cf667f22b0660656049c949538890b12
bassmadrigal <jebrhansen+SBo@gmail.com>
2016-05-28 22:50:45 -0400

    Intial repo creation

---
+++ mute_screenoff.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright 2016 Jeremy Hansen <jebrhansen -at- gmail.com>
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# This is a script to mute your master volume when the screen times out
+# and turns off. And unmute it when the screen turns back on. You can
+# also add additional commands to do additional things like pause your
+# music player. This should work for any distro that uses xorg and alsa.
+#
+# Originally developed for Slackware 14.1 -- Might need a new one for 14.2
+
+while true; do 
+
+  # Grab the current status of the monitor(s)
+  cur_monitor=$(xset -q | grep Monitor | cut -d' ' -f5)
+
+  # Check to see if there's a difference from the last run
+  if [[ $cur_monitor != $pre_monitor ]]
+  then
+
+    # Store the current volume
+    cur_volume=$(amixer get Master | grep "Left:" | cut -d' ' -f6)
+
+    # Checks if the monitor turned "Off" and the volume isn't muted
+    if [[ "$cur_monitor" == 'Off' && "$cur_volume" != '0' ]]
+    then
+      pre_volume=$cur_volume
+
+      # Mute the speakers when the monitor is turned off
+      # Add/replace your own commands here (pause music, etc)
+      amixer set Master 0
+    fi
+
+    # Checks if monitor turned "On" and if the volume was muted
+    if [[ "$cur_monitor" == 'On' && "$cur_volume" == '0' ]]
+    then
+
+      # Set speakers to previous volume
+      # Add/replace your own commands here (start music, etc)
+      amixer set Master $pre_volume
+    fi
+    
+  fi
+
+  # Sets the "previous" monitor for the next round  
+  pre_monitor=$cur_monitor
+
+  # Wait ten seconds then run the script again
+  sleep 10
+  
+done
---
+++ slack-mirror-speedtest.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# slack-mirror-speedtest.sh
+# Originally written for Ubuntu by Lance Rushing <lance_rushing@hotmail.com>
+# Dated 9/1/2006
+# Taken from http://ubuntuforums.org/showthread.php?t=251398
+# This script is covered under the GNU Public License: http://www.gnu.org/licenses/gpl.txt
+#
+# Heavily modified for Slackware by Jeremy Brent Hansen <jebrhansen -at- gmail.com>
+#
+# Changelog
+#
+# 2016/05/28 - Added additional comments for github upload
+# 2015/11/06 - Initial release of slack-mirror-speedtest.sh
+
+# Add or change mirrors from /etc/slackpkg/mirrors as desired (these are the US mirrors)
+# Ensure there is a beginning after the equal sign and an end quote after the final mirror
+MIRRORS="ftp://carroll.aset.psu.edu/pub/linux/distributions/slackware/slackware64-current/
+http://carroll.aset.psu.edu/pub/linux/distributions/slackware/slackware64-current/
+ftp://ftp.gtlib.gatech.edu/nv/ao2/lxmirror/ftp.slackware.com/slackware64-current/
+ftp://ftp.osuosl.org/.2/slackware/slackware64-current/
+http://ftp.osuosl.org/.2/slackware/slackware64-current/
+ftp://hpc-mirror.usc.edu/pub/linux/distributions/slackware/slackware64-current/
+http://hpc-mirror.usc.edu/pub/linux/distributions/slackware/slackware64-current/
+ftp://marmot.tn.utexas.edu/pub/slackware/slackware64-current/
+http://marmot.tn.utexas.edu/slackware/slackware64-current/
+ftp://mirror.cs.princeton.edu/pub/mirrors/slackware/slackware64-current/
+http://mirror.metrocast.net/slackware/slackware64-current/
+ftp://mirrors.easynews.com/linux/slackware/slackware64-current/
+http://mirrors.easynews.com/linux/slackware/slackware64-current/
+http://mirrors.kingrst.com/slackware/slackware64-current/   
+ftp://mirrors.us.kernel.org/slackware/slackware64-current/
+http://mirrors.us.kernel.org/slackware/slackware64-current/
+ftp://mirrors.xmission.com/slackware/slackware64-current/
+http://mirrors.xmission.com/slackware/slackware64-current/
+http://slackbuilds.org/mirror/slackware/slackware64-current/ 
+http://slackware.cs.utah.edu/pub/slackware/slackware64-current/
+http://slackware.mirrorcatalogs.com/slackware64-current/
+http://slackware.mirrors.pair.com/slackware64-current/
+ftp://slackware.mirrors.tds.net/pub/slackware/slackware64-current/
+http://slackware.mirrors.tds.net/pub/slackware/slackware64-current/
+ftp://slackware.virginmedia.com/mirrors/ftp.slackware.com/slackware64-current/
+http://slackware.virginmedia.com/slackware64-current/
+ftp://spout.ussg.indiana.edu/linux/slackware/slackware64-current/
+http://spout.ussg.indiana.edu/linux/slackware/slackware64-current/
+ftp://teewurst.cc.columbia.edu/pub/linux/slackware/slackware64-current/
+http://teewurst.cc.columbia.edu/pub/linux/slackware/slackware64-current/"
+
+# Use any adequetly sized file to test the speed. This is ~7MB.
+# The location should be based on the relative location within
+# the slackware64-current tree. I originally tried a smaller 
+# file (FILELIST.TXT ~1MB), but I was seeing slower speed results
+# since it didn't have time to fully max my connection. Depending
+# on your internet speed, you may want to try different sized files.
+FILE="kernels/huge.s/bzImage"
+
+# Number of seconds before the test is considered a failure
+TIMEOUT="5"
+
+# Clear the string that results are stored in
+RESULTS=""
+
+# Set color variables to make results and echo statements cleaner
+RED="\e[31m"
+GREEN="\e[32m"
+NC="\e[0m"  #No color
+
+for MIRROR in $MIRRORS ; do
+	
+  echo -n "Testing ${MIRROR} "
+	
+  # Combine the mirror with the file to create the URL
+  URL="${MIRROR}${FILE}"
+
+  # Time the download of the file
+  SPEED=$(curl --max-time $TIMEOUT --silent --output /dev/null --write-out %{speed_download} $URL)
+
+  # If the speed is below ~10kb/s mark it as a failure
+  if (( $(echo "$SPEED < 10000.000" | bc -l) )) ; then
+    echo -e "${RED}Fail${NC}";
+  else 
+    # Use numfmt to convert to a pretty speed
+    SPEED="$(numfmt --to=iec-i --suffix=B --padding=7 $SPEED)ps"
+    echo -e "${GREEN}$SPEED${NC}"
+    RESULTS="${RESULTS}\t${SPEED}\t${MIRROR}\n";
+  fi
+
+done;
+
+# Display a sorted list to the prompt
+echo -e "\nResults:"
+echo -e $RESULTS | sort -hr
