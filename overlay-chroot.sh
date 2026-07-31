212b5a689685de616658b444b845cd35f4b292e3
Jeremy Hansen <jebrhansen+github@gmail.com>
2026-07-30 17:34:45 -0700

    overlay-chroot.sh: Automate local mirror and base installation creation
    Allow a new system to run overlay-chroot without requiring the local
    Slackware mirror or base installation to be created beforehand.
    
    This commit:
    - Derives full mirror and base paths from ARCH and VERSION
    - Creates a local Slackware mirror with rsync
      - Excludes source trees by default
      - Detects stale mirror and offers to update it
    - Creates the base Slackware installation if missing
      - Prefers patched packages when available

--- overlay-chroot.sh
+++ overlay-chroot.sh
@@ -44,16 +44,39 @@
 CHROOT_LOCATION=/tmp/
 CHROOT_TEMPLATE_BASE="chroot"
 
-# Set variables for the base image and location of the local Slackware mirror
+
+# This script defaults to 64bit Slackware. If you want a 32bit chroot, remove
+# the 64 from the following variable (so it is unset).
+ARCH="64"
+
+# Set your Slackware version
 VERSION=15.0
-SLACKWARE_BASE=/share/gothrough/sbo-build/$VERSION
-LOCAL_MIRROR=/share/gothrough/slackware-mirrors/slackware64-$VERSION/
+
+# Set the parent locations for the base install and local mirror
+# NOTE: Do not add the slackware repo to the variables
+SLACKWARE_BASE=/share/gothrough/sbo-build/
+LOCAL_MIRROR=/share/gothrough/slackware-mirrors/
+
+# The default rsync mirror when setting up an initial local mirror
+# NOTE: Do not add the slackware repo to the variables
+RSYNC_MIRROR=rsync://mirror.slackbuilds.org/slackware/
+
+# Default to exclude source dirs, which should save time in the original sync
+# and in subsequent re-syncs. Change to "no" if you want sources included.
+# Options are "yes" or "no" with the unset default being "no".
+# Excluding the source saves about 12GB, making the final size around 8GB,
+# where the full source is around 20GB.
+EXCLUDE_SOURCE="yes"
 
 # To allow you to open GUI programs from within the chroot, you need to
 # allow "remote" access to the x server. This could possibly open up
 # security issues, but it is limited to non-network local connections.
 # Change to "no" if you want this disabled or pass ACCESS=no to the script.
 ACCESS=${ACCESS:-yes}
+
+# Let's set some default rsync options that might get updated in later parts
+# of the script.
+RSYNC_OPTIONS="-a --delete"
 
 # ---------------------------Global Settings Ending----------------------------
 
@@ -66,6 +89,20 @@ custom_cmd='
 alias checkdeps=". *.info; for i in \$REQUIRES; do ls /var/log/packages/*SBo* | cut -d/ -f5 | rev | cut -d- -f4- | rev | grep ^\$i$; done"
 '
 # ---------------------------Custom Commands Ending----------------------------
+
+# -------------------------Derived Settings Beginning--------------------------
+
+# Use ARCH/VERSION variables to set the SLACKWARE_REPO variable
+SLACKWARE_REPO="slackware$ARCH-$VERSION"
+
+# Add the Slackware repository to the configured locations
+SLACKWARE_BASE="${SLACKWARE_BASE%/}/$SLACKWARE_REPO"
+LOCAL_MIRROR="${LOCAL_MIRROR%/}/$SLACKWARE_REPO"/
+
+# The default rsync mirror when setting up an initial local mirror
+RSYNC_MIRROR="${RSYNC_MIRROR%/}/$SLACKWARE_REPO"/
+
+# ---------------------------Derived Settings Ending---------------------------
 
 # Simplify calling umount
 safe_umount ()
@@ -123,6 +160,126 @@ if [ "$1" == "cleanup" ]; then
     echo "Cleanup complete"
   fi
   exit
+fi
+
+# If EXCLUDE_SOURCE was set to "yes" include options for rsync to skip the
+# source dirs of the main distro and in subfolders (which includes extra/,
+# patches/, pasture/, and testing/.
+if [ "$EXCLUDE_SOURCE" == "yes" ]; then
+  RSYNC_OPTIONS="$RSYNC_OPTIONS --exclude=source/ --exclude=*/source/"
+fi
+
+# Check for the local mirror
+if [ ! -d "$LOCAL_MIRROR" ] || [ ! -f "$LOCAL_MIRROR/ChangeLog.txt" ]; then
+  echo "It seems you do not have a local Slackware mirror."
+  read -erp "Would you like to set it up? (Warning this will take some time and requires internet) Y/n " answer
+  if /usr/bin/grep -qi "n" <<< "$answer"; then
+    exit 1
+  fi
+
+  # Sync everything into a temporary folder. Later, if rsync succeeds, move it
+  # over to the actual mirror directory.
+  TMP_MIRROR="${LOCAL_MIRROR%/}.partial.$$"
+  mkdir -p "$TMP_MIRROR"
+
+  echo "Syncing mirror..."
+
+  if rsync $RSYNC_OPTIONS -- "$RSYNC_MIRROR/" "$TMP_MIRROR"; then
+
+    # Make sure we're syncing into an empty directory
+    if [ -d "$LOCAL_MIRROR" ]; then
+      rm -rf -- "$LOCAL_MIRROR"
+    fi
+
+    mv "$TMP_MIRROR" "${LOCAL_MIRROR%/}"
+
+    echo "Please set up a crontab to keep this mirror up-to-date."
+    echo "As root, run \`crontab -e\` and add the following line to run a daily 3AM sync:"
+    echo "0 3 * * * rsync $RSYNC_OPTIONS -- \"$RSYNC_MIRROR/\" \"$LOCAL_MIRROR\""
+
+  else
+    echo "rsync failed or did not complete. Please try manually."
+    echo "rsync $RSYNC_OPTIONS -- \"$RSYNC_MIRROR\" \"$LOCAL_MIRROR\""
+    rm -rf -- "$TMP_MIRROR"
+  fi
+fi
+
+# Check for base install
+if [ ! -d "$SLACKWARE_BASE" ] || [ ! -f "$SLACKWARE_BASE"/etc/slackware-version ]; then
+  echo "It seems the base Slackware install is not present."
+  read -erp "Would you like to create it? Y/n " answer
+  if /usr/bin/grep -qi "n" <<< "$answer"; then
+    exit 1
+  fi
+
+  # Check that a single mirror is selected for slackpkg
+  slackpkg_mirror=$(grep -vE '^[[:space:]]*(#|$)' /etc/slackpkg/mirrors)
+  if [ "$(echo "$slackpkg_mirror" | wc -l)" -ne "1" ]; then
+    echo "slackpkg does not have a mirror set properly."
+    echo "Please check /etc/slackpkg/mirrors and try again."
+    exit 2
+  elif ! echo "$slackpkg_mirror" | grep -q -- "-$VERSION/"; then
+    echo "The selected mirror does not match the Slackware version for this script."
+    exit 3
+  fi
+
+  mkdir -p "$SLACKWARE_BASE"
+
+  # Get a list of packages in patches/ and use those instead of installing
+  # the original packages. This should prevent needing to upgrade all patched
+  # packages when actually creating the chroot.
+  declare -A PATCHES
+  for pkg in "$LOCAL_MIRROR"/patches/packages/*.t?z; do
+    basepkg=$(basename "$pkg")
+    pkgname=$(echo "$basepkg" | rev | cut -d- -f4- | rev)
+    PATCHES["$pkgname"]="$pkg"
+  done
+
+  # Loop through all the packages and install patched when available or
+  # original when no patched versions exist.
+  for pkg in "$LOCAL_MIRROR"/slackware"${ARCH}"/*/*.t?z; do
+
+    basepkg=$(basename "$pkg")
+    pkgname=$(echo "$basepkg" | rev | cut -d- -f4- | rev)
+
+    # Use patched package if available
+    if [ -n "${PATCHES[$pkgname]}" ]; then
+      install_pkg="${PATCHES[$pkgname]}"
+      unset "PATCHES[$pkgname]"
+    else
+      install_pkg="$pkg"
+    fi
+    if ! ROOT="$SLACKWARE_BASE" installpkg "$install_pkg"; then
+      echo "installpkg failed for: $install_pkg"
+      exit 2
+    fi
+
+  done
+
+  # Catch any added packages to stable in patches/
+  for i in "$LOCAL_MIRROR"/patches/packages/*.t?z; do
+    package=$(echo "$i" | rev | cut -d/ -f1 | cut -d. -f2 | rev)
+    if [ ! -f "$SLACKWARE_BASE"/var/log/packages/"$package" ]; then
+      ROOT="$SLACKWARE_BASE" installpkg "$i"
+    fi
+  done
+fi
+
+# Check if the local mirror is up-to-date and if not, prompt to update it
+REMOTE_CHANGELOG="https://mirror.slackbuilds.org/slackware/$SLACKWARE_REPO/ChangeLog.txt"
+status=$(curl -s -o /dev/null -w '%{http_code}' \
+              -z "$LOCAL_MIRROR/ChangeLog.txt" \
+              "$REMOTE_CHANGELOG")
+
+if [ "$status" == "304" ]; then
+  echo "Slackware mirror up-to-date"
+elif [ "$status" == "200" ]; then
+  read -erp "Local Slackware mirror out of date. Would you like to update it? Y/n " answer
+  if ! /usr/bin/grep -qi "n" <<< "$answer"; then
+    if ! rsync $RSYNC_OPTIONS --delay-updates -- "$RSYNC_MIRROR/" "$LOCAL_MIRROR"; then
+      echo "rsync of mirror failed. You are not running the latest Slackware in this chroot!"
+    fi
+  fi
 fi
 
 # Track the latest updates to prevent attempting to update system
